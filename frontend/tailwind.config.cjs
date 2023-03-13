50cbdf530fa2b366d01aa937634234763d624158
Farese, John <faresejohn@gmail.com>
2023-03-13 17:00:32 -0400

    Update color palette

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -5,7 +5,12 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#20484F',
+        secondary: '#B8E4DA',
+      }
+    },
   },
   plugins: [],
 }
