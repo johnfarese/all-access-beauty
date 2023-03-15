7275a66f3b60b4b905d112f007446642c4ad36a6
Farese, John <faresejohn@gmail.com>
2023-03-15 17:11:36 -0400

    Create Select component and add to Filters section

---
+++ frontend/src/components/Select.jsx
@@ -0,0 +1,26 @@
+const Select = (props) => {
+
+    const getOptions = () => {
+        return props.options.map((option) => {
+            return <option key={option.value} value={option.value}>{option.name}</option>;
+        });
+    };
+
+    return (
+        <div>
+            <label htmlFor={props.id} className="block text-sm text-left font-medium leading-6 text-gray-900">
+                {props.name}
+            </label>
+            <select
+                id={props.id}
+                name={props.id}
+                className="mt-1 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-primary sm:text-sm sm:leading-6"
+                defaultValue={props.defaultValue}
+            >
+                {getOptions()}
+            </select>
+        </div>
+    );
+};
+
+export default Select;
