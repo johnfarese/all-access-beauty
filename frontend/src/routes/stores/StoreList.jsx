02471aedc9833407e57adcadfcf808ce59306487
Farese, John <faresejohn@gmail.com>
2023-03-15 11:27:24 -0400

    Add brand, full address, and google link to store cards

--- frontend/src/routes/stores/StoreList.jsx
+++ frontend/src/routes/stores/StoreList.jsx
@@ -18,9 +18,12 @@ const StoreList = (props) => {
         >
             {stores.map((store) => (
                 <li key={store.store_id} className="rounded-2xl bg-white border border-gray-200 py-10 px-8">
-                    <img className="mx-auto h-48 w-48 rounded-md md:h-56 md:w-56" src={imageMap[store.brand]} alt="" />
-                    <h3 className="mt-6 text-base font-semibold leading-7 tracking-tight">{store.store_name}</h3>
-                    <p className="text-sm leading-6 text-gray-400">{`${store.address.city}, ${store.address.state}`}</p>
+                    <img className="mx-auto h-48 w-48 rounded-md md:h-56 md:w-56" src={imageMap[store.brand.toLowerCase()]} alt="" />
+                    <h3 className="mt-6 text-base font-semibold leading-7 tracking-tight">{`${store.brand} ${store.store_name}`}</h3>
+                    <a href={store.google_link} className="text-sm leading-6 text-gray-400" target="_blank">
+                        <span className="block xl:inline text-sm leading-6 text-gray-400">{`${store.address.street_number} ${store.address.street_name}`}</span>{' '}
+                        <span className="block xl:inline text-sm leading-6 text-gray-400">{`${store.address.city}, ${store.address.state}`}</span>
+                    </a>
                     <AccessibleFeatures features={store.accessibility_features} />
                 </li>
             ))}
