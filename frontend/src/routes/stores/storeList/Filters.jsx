f72ef13b1600577b56950cd0ded32389fdbad253
Farese, John <faresejohn@gmail.com>
2023-03-16 11:26:58 -0400

    Update to enable filtering of store list

--- frontend/src/routes/stores/storeList/Filters.jsx
+++ frontend/src/routes/stores/storeList/Filters.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Select from '../../../components/Select';
 
 const brandOptions = [
@@ -7,26 +8,78 @@ const brandOptions = [
     },
     {
         name: "Aveda",
-        value: "aveda"
+        value: "Aveda"
     },
     {
         name: "Jo Malone",
-        value: "jomalone"
+        value: "Jo Malone"
     },
     {
         name: "MAC",
-        value: "mac"
+        value: "MAC"
     },
     {
         name: "Origins",
-        value: "origins"
+        value: "Origins"
     }
 ];
 
-const Filters = () => {
+const featureOptions = [
+    {
+        name: "Any",
+        value: "*"
+    },
+    {
+        name: "Accessible Parking",
+        value: "Accessible parking"
+    },
+    {
+        name: "Accessible Restroom",
+        value: "Accessible restroom"
+    },
+    {
+        name: "Assistive Listening System",
+        value: "Assistive listening system"
+    },
+    {
+        name: "Braille",
+        value: "Braille"
+    },
+    {
+        name: "Elevator Access",
+        value: "Elevator access"
+    },
+    {
+        name: "Sign Language",
+        value: "Sign language"
+    },
+    {
+        name: "Text-to-Speech Software",
+        value: "Text-to-speech software"
+    },
+    {
+        name: "Wheelchair Accessible",
+        value: "Wheelchair accessible"
+    }
+];
+
+const Filters = (props) => {
+
+    const handleChange = (e) => {
+        const value = e.target.value;
+        const name = e.target.name;
+        props.setFilters((prevalue) => {
+            return {
+                ...prevalue,
+                [name]: value
+            }
+        });
+    };
+
     return (
         <div className="mx-auto grid max-w-2xl grid-cols-1 gap-6 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-4 lg:gap-8">
-            <Select id="brand" name="Brand" options={brandOptions} defaultValue="*" />
+            <Select id="brand" name="Brand" handler={handleChange} options={brandOptions} defaultValue="*" />
+            <Select id="features" name="Accessibility Features" handler={handleChange} options={featureOptions} defaultValue="*" />
         </div>
     );
 };
